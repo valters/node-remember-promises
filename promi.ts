19c35f8cf35893a4eb9f6b814ce6f9a0b2a4c1b0
Valters Vingolds <valters@vingolds.ch>
2023-04-03 12:07:18 +0200

    improve handling examples

--- promi.ts
+++ promi.ts
@@ -13,9 +13,9 @@ const example = async () => {
   } catch (err) {
     console.log("CAUGHT: " + err);
     // CRASH: try different ways of handling/rethrowing the error here
-    //throw e;
-    throw new Error("example also fails: " + err);
-    //return Promise.resolve("error handled" + err);
+    //throw err;
+    //throw new Error("example also fails: " + err);
+    return Promise.resolve("error handled: " + err);
   }
   return Promise.resolve("ok");
 };
